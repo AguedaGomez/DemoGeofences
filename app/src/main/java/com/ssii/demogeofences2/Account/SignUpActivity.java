7c183064edac23519ff34e029ab4fd961405af86
Agueda Gomez Cambronero <al285895@uji.es>
2018-08-21 15:02:39 +0200

    Incluye sign up activity

---
+++ app/src/main/java/com/ssii/demogeofences2/Account/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.ssii.demogeofences2.Account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ssii.demogeofences2.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
